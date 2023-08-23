c4c2c2917823e995babe722116f9f80e2df43ce9
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-08-23 13:49:44 +0200

    feat: add button component (#19)

---
+++ .changeset/afraid-steaks-give.md
@@ -0,0 +1,5 @@
+---
+"@zenml-io/react-component-library": minor
+---
+
+add initial version of the button component to the library
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { cn } from "./utilities/index";
 export { zenmlPlugin, zenmlPreset } from "./tailwind/index";
+export { Button, ButtonProps } from "./components/button/Button";
