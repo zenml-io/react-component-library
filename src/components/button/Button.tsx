d7908ae7c4117310c060b69fa55fd62c8e4e2feb
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-08-23 08:43:27 +0000

    feat: add cva

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,13 +1,13 @@
 import React, { ButtonHTMLAttributes } from "react";
+import { type VariantProps, cva } from "class-variance-authority";
 import { cn } from "../../utilities/index";
 
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+const buttonVariants = cva("bg-primary-500 rounded-md py-1 px-3 text-theme-text-negative");
+
+export interface ButtonProps
+	extends ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonVariants> {}
 
 export function Button({ className, ...rest }: ButtonProps) {
-	return (
-		<button
-			className={cn("bg-primary-500 rounded-md py-1 px-3 text-theme-text-negative", className)}
-			{...rest}
-		/>
-	);
+	return <button className={cn(buttonVariants(), className)} {...rest} />;
 }
