b71c20d2d7a940ba261748543aef7607a563de86
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-10-19 14:57:44 +0200

    refactor: use forwardRef

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,86 +1,113 @@
 import React, {
 	HTMLAttributes,
+	HTMLProps,
 	PropsWithChildren,
 	ReactNode,
 	cloneElement,
+	forwardRef,
 	isValidElement
 } from "react";
-import { Slot, Slottable } from "@radix-ui/react-slot";
+import { Slot } from "@radix-ui/react-slot";
 import { cn } from "../../utilities/index";
 
-export function Sidebar({ className, children, ...rest }: HTMLAttributes<HTMLElement>) {
-	return (
-		<nav
-			className={cn(
-				"group flex-1 h-full flex w-9 hover:w-[220px] bg-neutral-100 transition-all overflow-x-hidden duration-300 flex-col items-center border-r border-theme-border-moderate",
-				className
-			)}
-			{...rest}
-		>
-			<div className="flex flex-col h-full flex-1 w-full">{children}</div>
-		</nav>
-	);
-}
+export const Sidebar = forwardRef<HTMLDivElement, HTMLProps<HTMLDivElement>>(
+	({ className, children, ...rest }, ref) => {
+		return (
+			<nav
+				ref={ref}
+				className={cn(
+					"group flex-1 h-full flex w-9 hover:w-[220px] bg-neutral-100 transition-all overflow-x-hidden duration-300 flex-col items-center border-r border-theme-border-moderate",
+					className
+				)}
+				{...rest}
+			>
+				<div className="flex flex-col h-full flex-1 w-full">{children}</div>
+			</nav>
+		);
+	}
+);
 
-type SidebarHeaderProps = {
-	title: string;
-	icon?: React.ReactNode;
-};
+Sidebar.displayName = "Sidebar";
 
 export function SidebarHeaderImage({ children }: PropsWithChildren) {
 	return <Slot className="w-6 h-6 rounded-sm shrink-0">{children}</Slot>;
 }
 
-export function SidebarHeader({ title, icon, children }: PropsWithChildren<SidebarHeaderProps>) {
-	const existingIconClasses = isValidElement(icon) ? icon.props.className || "" : "";
+export type SidebarHeaderProps = HTMLAttributes<HTMLDivElement> & {
+	title: string;
+	icon?: React.ReactNode;
+};
 
-	const iconClasses = cn(
-		existingIconClasses,
-		"w-6 ml-auto shrink-0 h-6 opacity-0 group-hover:opacity-100  duration-300 transition-all"
-	);
+export const SidebarHeader = forwardRef<HTMLDivElement, SidebarHeaderProps>(
+	({ title, icon, children, className, ...rest }, ref) => {
+		const existingIconClasses = isValidElement(icon) ? icon.props.className || "" : "";
 
-	return (
-		<div className="bg-theme-surface-primary flex items-center whitespace-nowrap p-3 border-b border-theme-border-moderate">
-			{children}
-			<p className="opacity-0 group-hover:opacity-100 ml-1 truncate duration-300 transition-all">
-				{title}
-			</p>
-			{icon && cloneElement(icon as React.ReactElement, { className: iconClasses })}
-		</div>
-	);
-}
+		const iconClasses = cn(
+			existingIconClasses,
+			"w-6 ml-auto shrink-0 h-6 opacity-0 group-hover:opacity-100  duration-300 transition-all"
+		);
 
-export function SidebarBody({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-	return <div className={cn(`flex-1 flex flex-col w-full`, className)} {...rest} />;
-}
-
-export function SidebarList({ className, ...rest }: HTMLAttributes<HTMLUListElement>) {
-	return (
-		<ul
-			className={cn("px-1 py-2 flex gap-0.5 w-full flex-col items-center", className)}
-			{...rest}
-		/>
-	);
-}
-
-export function SidebarItem({
-	isActive = false,
-	...rest
-}: {
-	children: ReactNode;
-	isActive?: boolean;
-}) {
-	return (
-		<li className="w-full">
-			<Slot
-				className={`flex p-2 items-center gap-2 rounded-md w-full ${
-					isActive ? "bg-theme-surface-primary" : "hover:bg-neutral-200 active:bg-neutral-300"
-				}`}
+		return (
+			<div
+				ref={ref}
 				{...rest}
-			></Slot>
-		</li>
-	);
-}
+				className={cn(
+					"bg-theme-surface-primary flex items-center whitespace-nowrap p-3 border-b border-theme-border-moderate",
+					className
+				)}
+			>
+				{children}
+				<p className="opacity-0 group-hover:opacity-100 ml-1 truncate duration-300 transition-all">
+					{title}
+				</p>
+				{icon && cloneElement(icon as React.ReactElement, { className: iconClasses })}
+			</div>
+		);
+	}
+);
+
+SidebarHeader.displayName = "SidebarHeader";
+
+export const SidebarBody = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+	({ className, ...rest }, ref) => {
+		return <div ref={ref} className={cn(`flex-1 flex flex-col w-full`, className)} {...rest} />;
+	}
+);
+
+export const SidebarList = forwardRef<HTMLUListElement, HTMLAttributes<HTMLUListElement>>(
+	({ className, ...rest }, ref) => {
+		return (
+			<ul
+				ref={ref}
+				className={cn("px-1 py-2 flex gap-0.5 w-full flex-col items-center", className)}
+				{...rest}
+			/>
+		);
+	}
+);
+
+SidebarList.displayName = "SidebarList";
+
+export type SidebarItemProps = HTMLAttributes<HTMLLIElement> & {
+	isActive?: boolean;
+};
+
+export const SidebarItem = forwardRef<HTMLLIElement, SidebarItemProps>(
+	({ isActive, className, ...rest }, ref) => {
+		return (
+			<li ref={ref} className={cn("w-full", className)}>
+				<Slot
+					className={`flex p-2 items-center gap-2 rounded-md w-full ${
+						isActive ? "bg-theme-surface-primary" : "hover:bg-neutral-200 active:bg-neutral-300"
+					}`}
+					{...rest}
+				></Slot>
+			</li>
+		);
+	}
+);
+
+SidebarItem.displayName = "SidebarItem";
 
 type SidebarItemContentProps = {
 	icon: React.ReactNode;
