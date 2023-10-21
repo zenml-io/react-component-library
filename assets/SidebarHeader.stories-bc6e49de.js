051cf34c33fbbb4ca389e517b5bd02d74e10638b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-10-21 14:06:16 +0000

    Deploying to gh-pages from  @ ab3189e30acb0f733b8ed949c5c9ed4323718b4d 🚀

---
+++ assets/SidebarHeader.stories-bc6e49de.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-29545a09.js";import{b as o,c as d,d as i}from"./icons-1a06cf8f.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-b5922a48.js";import"./extends-98964cd2.js";const h={title:"UI/Sidebar",component:o,decorators:[s=>e("div",{className:"group",children:e(s,{})})],tags:["autodocs"]},a={name:"Sidebar Header",args:{title:"ZenML Tenant",icon:e(d,{}),children:e(i,{children:e("img",{src:"https://avatar.vercel.sh/ZenMLTenant?size=32"})})}};var r,t,n;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: "Sidebar Header",
+  args: {
+    title: "ZenML Tenant",
+    icon: <CloseButton />,
+    children: <SidebarHeaderImage>
+                <img src={\`https://avatar.vercel.sh/ZenMLTenant?size=32\`} />
+            </SidebarHeaderImage>
+  }
+}`,...(n=(t=a.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const u=["sidebarHeader"];export{u as __namedExportsOrder,h as default,a as sidebarHeader};
+//# sourceMappingURL=SidebarHeader.stories-bc6e49de.js.map
