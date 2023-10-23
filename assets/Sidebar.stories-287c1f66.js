ad9223a1d6cb30b23a21d0e515519975e00fa6e2
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-10-23 15:03:08 +0000

    Deploying to gh-pages from  @ d5410141d601671993dd937687e07957ae51e13d 🚀

---
+++ assets/Sidebar.stories-287c1f66.js
@@ -0,0 +1,48 @@
+import{a as r,j as e,F as p}from"./jsx-runtime-29545a09.js";import{e as m,b as S,c as b,d as _,f as y,g as u,h as v,S as i,a as t,C as d}from"./icons-965b0098.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-b5922a48.js";import"./extends-98964cd2.js";try{SidebarHeaderImage.displayName="SidebarHeaderImage",SidebarHeaderImage.__docgenInfo={description:"",displayName:"SidebarHeaderImage",props:{}}}catch{}try{SidebarItem.displayName="SidebarItem",SidebarItem.__docgenInfo={description:"",displayName:"SidebarItem",props:{isActive:{defaultValue:{value:"false"},description:"",name:"isActive",required:!1,type:{name:"boolean"}}}}}catch{}try{SidebarItemContent.displayName="SidebarItemContent",SidebarItemContent.__docgenInfo={description:"",displayName:"SidebarItemContent",props:{icon:{defaultValue:null,description:"",name:"icon",required:!0,type:{name:"ReactNode"}},label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}},isActive:{defaultValue:null,description:"",name:"isActive",required:!1,type:{name:"boolean"}},svgStroke:{defaultValue:{value:"false"},description:"",name:"svgStroke",required:!1,type:{name:"boolean"}}}}}catch{}try{Sidebar.displayName="Sidebar",Sidebar.__docgenInfo={description:"",displayName:"Sidebar",props:{}}}catch{}try{SidebarHeaderTitle.displayName="SidebarHeaderTitle",SidebarHeaderTitle.__docgenInfo={description:"",displayName:"SidebarHeaderTitle",props:{}}}catch{}try{SidebarHeader.displayName="SidebarHeader",SidebarHeader.__docgenInfo={description:"",displayName:"SidebarHeader",props:{icon:{defaultValue:null,description:"",name:"icon",required:!1,type:{name:"ReactNode"}}}}}catch{}try{SidebarBody.displayName="SidebarBody",SidebarBody.__docgenInfo={description:"",displayName:"SidebarBody",props:{}}}catch{}try{SidebarList.displayName="SidebarList",SidebarList.__docgenInfo={description:"",displayName:"SidebarList",props:{}}}catch{}function l({children:a}){return r("div",{style:{minHeight:"100vh",display:"flex",flexDirection:"column",width:"100%"},children:[e("div",{style:{height:"64px",backgroundColor:"white",borderBottom:"solid lightgrey 1px"}}),a]})}try{l.displayName="AppShell",l.__docgenInfo={description:"",displayName:"AppShell",props:{}}}catch{}const A={title:"UI/Sidebar",component:m,parameters:{layout:"fullscreen"},decorators:[a=>e(l,{children:e(a,{})})],tags:["autodocs"]},n={name:"Sidebar",args:{children:r(p,{children:[r(S,{icon:e(b,{className:"w-6 h-6"}),title:"ZenML Tenant",children:[e(_,{children:e("img",{src:"https://avatar.vercel.sh/ZenMLTenant?size=32"})}),e(y,{children:"My Tenant"})]}),r(u,{children:[r(v,{children:[e("li",{className:"w-full",children:e(i,{isActive:!0,children:e("div",{children:e(t,{svgStroke:!0,isActive:!0,icon:e(d,{}),label:"Models"})})})}),e("li",{className:"w-full",children:e(i,{isActive:!1,children:e("div",{children:e(t,{svgStroke:!0,isActive:!1,icon:e(d,{}),label:"Models"})})})}),e("li",{className:"w-full",children:e(i,{isActive:!1,children:e("div",{children:e(t,{svgStroke:!0,isActive:!1,icon:e(d,{}),label:"Models"})})})})]}),e("div",{style:{marginTop:"auto"},children:e(i,{children:e("div",{children:e(t,{svgStroke:!0,isActive:!1,icon:e(d,{}),label:"Models"})})})})]})]})}};var s,o,c;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: "Sidebar",
+  args: {
+    children: <>
+                <SidebarHeader icon={<CloseButton className="w-6 h-6" />} title="ZenML Tenant">
+                    <SidebarHeaderImage>
+                        <img src={\`https://avatar.vercel.sh/ZenMLTenant?size=32\`} />
+                    </SidebarHeaderImage>
+                    <SidebarHeaderTitle>My Tenant</SidebarHeaderTitle>
+                </SidebarHeader>
+                <SidebarBody>
+                    <SidebarList>
+                        <li className="w-full">
+                            <SidebarItem isActive={true}>
+                                <div>
+                                    <SidebarItemContent svgStroke isActive={true} icon={<CPU />} label="Models" />
+                                </div>
+                            </SidebarItem>
+                        </li>
+                        <li className="w-full">
+                            <SidebarItem isActive={false}>
+                                <div>
+                                    <SidebarItemContent svgStroke isActive={false} icon={<CPU />} label="Models" />
+                                </div>
+                            </SidebarItem>
+                        </li>
+                        <li className="w-full">
+                            <SidebarItem isActive={false}>
+                                <div>
+                                    <SidebarItemContent svgStroke isActive={false} icon={<CPU />} label="Models" />
+                                </div>
+                            </SidebarItem>
+                        </li>
+                    </SidebarList>
+                    <div style={{
+          marginTop: "auto"
+        }}>
+                        <SidebarItem>
+                            <div>
+                                <SidebarItemContent svgStroke isActive={false} icon={<CPU />} label="Models" />
+                            </div>
+                        </SidebarItem>
+                    </div>
+                </SidebarBody>
+            </>
+  }
+}`,...(c=(o=n.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};const C=["defaultStory"];export{C as __namedExportsOrder,A as default,n as defaultStory};
+//# sourceMappingURL=Sidebar.stories-287c1f66.js.map
---
+++ assets/SidebarHeader.stories-eb57465b.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-29545a09.js";import{b as o,c as d,d as i}from"./icons-965b0098.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-b5922a48.js";import"./extends-98964cd2.js";const h={title:"UI/Sidebar",component:o,decorators:[n=>e("div",{className:"group",children:e(n,{})})],tags:["autodocs"]},a={name:"Sidebar Header",args:{title:"ZenML Tenant",icon:e(d,{className:"w-6 h-6"}),children:e(i,{children:e("img",{src:"https://avatar.vercel.sh/ZenMLTenant?size=32"})})}};var r,s,t;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: "Sidebar Header",
+  args: {
+    title: "ZenML Tenant",
+    icon: <CloseButton className="w-6 h-6" />,
+    children: <SidebarHeaderImage>
+                <img src={\`https://avatar.vercel.sh/ZenMLTenant?size=32\`} />
+            </SidebarHeaderImage>
+  }
+}`,...(t=(s=a.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const u=["sidebarHeader"];export{u as __namedExportsOrder,h as default,a as sidebarHeader};
+//# sourceMappingURL=SidebarHeader.stories-eb57465b.js.map
