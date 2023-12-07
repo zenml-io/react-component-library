7e9257fa273a72f599bcaae59fc51352c1533977
Atzin Escandia <atzin.ext@zenml.io>
2023-12-07 17:45:09 +0100

    Improve badge styles

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -9,39 +9,52 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+	args: {
+		children: "Text",
+		rounded: false
+	}
+};
 
 export const Capitalized: Story = {
 	args: {
-		capitalized: true
+		children: "Capitalized",
+		capitalized: true,
+		rounded: true
 	}
 };
 
 export const SuccessVariant: Story = {
 	args: {
-		variant: "success"
+		children: "Success",
+		variant: "success",
+		rounded: false
 	}
 };
 
 export const WarningVariant: Story = {
 	args: {
+		children: "Warning",
 		variant: "warning"
 	}
 };
 
 export const InfoVariant: Story = {
 	args: {
+		children: "Info",
 		variant: "info"
 	}
 };
 export const GrayVariant: Story = {
 	args: {
+		children: "Gray",
 		variant: "gray"
 	}
 };
 
 export const DisabledVariant: Story = {
 	args: {
+		children: "Failed",
 		variant: "disabled"
 	}
 };
--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -3,36 +3,45 @@ import { HTMLAttributes } from "react";
 import { cn } from "../../utilities/index";
 import React from "react";
 
-const badge = cva(
-	"inline-flex h-5 items-center rounded-rounded px-2 py-0.5 text-text-xs font-semibold",
-	{
-		variants: {
-			capitalized: {
-				true: "uppercase",
-				false: ""
-			},
-			variant: {
-				default: "bg-primary-50 text-theme-text-brand",
-				success: "bg-success-100 text-success-800",
-				warning: "bg-warning-50 text-warning-800",
-				info: "bg-blue-50 text-blue-600",
-				gray: "bg-neutral-100 text-theme-text-secondary",
-				disabled: "bg-neutral-200 text-theme-text-secondary"
-			}
+const badge = cva("inline-flex h-5 items-center px-2 py-0.5 text-text-xs font-semibold", {
+	defaultVariants: {
+		variant: "gray",
+		capitalized: false,
+		rounded: true
+	},
+	variants: {
+		capitalized: {
+			true: "uppercase",
+			false: ""
 		},
-		defaultVariants: {
-			variant: "default",
-			capitalized: false
+		variant: {
+			default: "bg-primary-50 text-theme-text-brand",
+			success: "bg-success-100 text-success-800",
+			warning: "bg-warning-50 text-warning-800",
+			info: "bg-blue-50 text-blue-600",
+			gray: "bg-neutral-100 text-theme-text-secondary",
+			disabled: "bg-neutral-200 text-theme-text-secondary"
+		},
+		rounded: {
+			true: "rounded-full",
+			false: "rounded-sm"
 		}
 	}
-);
+});
 
 export type BadgeProps = VariantProps<typeof badge>;
 interface BadgeVariants extends HTMLAttributes<HTMLDivElement>, BadgeProps {}
 
-export const Badge = ({ children, className, variant, capitalized, ...rest }: BadgeVariants) => {
+export const Badge = ({
+	children,
+	className,
+	variant,
+	capitalized,
+	rounded,
+	...rest
+}: BadgeVariants) => {
 	return (
-		<div {...rest} className={cn(badge({ variant, capitalized }), className)}>
+		<div {...rest} className={cn(badge({ variant, capitalized, rounded }), className)}>
 			{children}
 		</div>
 	);
