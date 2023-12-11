6d38b3b80edfacb00c77a964e10f3d53f659ee09
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-12-11 16:34:17 +0000

    Deploying to gh-pages from  @ cdf718f97d506e26e117ba2ae6bbd923a8b581a7 🚀

---
+++ assets/SidebarItem.stories-782e8678.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-29545a09.js";import{S as v,a as d,C as m}from"./icons-f2ca59ad.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-b5922a48.js";import"./extends-98964cd2.js";const A={title:"UI/Sidebar",component:v,decorators:[l=>e("ul",{style:{listStyle:"none"},className:"group",children:e(l,{})})],tags:["autodocs"]},r={name:"Inactive Sidebar Item",args:{isActive:!1,children:e("div",{children:e(d,{svgStroke:!0,icon:e(m,{}),label:"Models"})})}},t={name:"Active Sidebar Item",args:{isActive:!0,children:e("div",{children:e(d,{svgStroke:!0,isActive:!0,icon:e(m,{}),label:"Models"})})}};var a,i,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: "Inactive Sidebar Item",
+  args: {
+    isActive: false,
+    children: <div>
+                <SidebarItemContent svgStroke icon={<CPU />} label="Models" />
+            </div>
+  }
+}`,...(s=(i=r.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var n,o,c;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: "Active Sidebar Item",
+  args: {
+    isActive: true,
+    children: <div>
+                <SidebarItemContent svgStroke isActive={true} icon={<CPU />} label="Models" />
+            </div>
+  }
+}`,...(c=(o=t.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};const h=["inactive","active"];export{h as __namedExportsOrder,t as active,A as default,r as inactive};
+//# sourceMappingURL=SidebarItem.stories-782e8678.js.map
---
+++ assets/Utilitiets-f03d037b.js
@@ -0,0 +1,8 @@
+import{j as e,a as n,F as r}from"./jsx-runtime-29545a09.js";import{M as a}from"./index-c3a7ebd0.js";import{u as s}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-89f2e15a.js";import"../sb-preview/runtime.js";import"./index-ffc7e5ff.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./index-356e4a49.js";function o(i){const t=Object.assign({h1:"h1",p:"p",code:"code",br:"br",h2:"h2"},s(),i.components);return n(r,{children:[e(a,{title:"Docs/Utilities"}),`
+`,e(t.h1,{id:"utilities",children:"Utilities"}),`
+`,n(t.p,{children:["The component library exposes a set of helpful utlitiy functions, that can be imported from ",e(t.code,{children:"@zenml-io/react-component-library/utilities"}),".",e(t.br,{}),`
+`,`The exposed utilities are decorated with JSDoc to improve the DX, and to make it as easy as possible to get
+started with them.`]}),`
+`,e(t.h2,{id:"functions",children:"Functions"}),`
+`,n(t.p,{children:[e(t.code,{children:"cn"})," : Utility function for merging multiple Tailwind classes, allowing the usage of the clsx-syntax. The abbreviation stands for ",e(t.code,{children:"classNames"})]})]})}function y(i={}){const{wrapper:t}=Object.assign({},s(),i.components);return t?e(t,Object.assign({},i,{children:e(o,i)})):o(i)}export{y as default};
+//# sourceMappingURL=Utilitiets-f03d037b.js.map
