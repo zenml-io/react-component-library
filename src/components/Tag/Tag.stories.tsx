48dc80cae57093a5b4ee09a8640c87f6e0770977
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-04-02 12:03:17 +0200

    feat: tag component (#52)

---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,83 @@
+import { Meta } from "@storybook/react";
+import { Tag } from "./index";
+import { StoryObj } from "@storybook/react";
+
+const meta = {
+	title: "Elements/Tag",
+	component: Tag,
+	argTypes: {
+		rounded: {
+			control: "boolean",
+			defaultValue: true
+		},
+		size: {
+			control: "select",
+			defaultValue: "sm",
+			options: ["sm", "xs"]
+		},
+		emphasis: {
+			control: "select",
+			defaultValue: "subtle",
+			options: ["bold", "subtle", "minimal"]
+		},
+		color: {
+			control: "select",
+			defaultValue: "blue",
+			options: [
+				"grey",
+				"purple",
+				"green",
+				"yellow",
+				"red",
+				"blue",
+				"teal",
+				"lime",
+				"magenta",
+				"turquoise",
+				"orange"
+			]
+		}
+	},
+	parameters: {
+		layout: "centered"
+	},
+
+	tags: ["autodocs"]
+} satisfies Meta<typeof Tag>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const minimal: Story = {
+	name: "Minimal",
+	args: {
+		size: "sm",
+		rounded: true,
+		emphasis: "minimal",
+		color: "magenta",
+		children: "Classified"
+	}
+};
+
+export const subtle: Story = {
+	name: "Subtle",
+	args: {
+		size: "sm",
+		rounded: true,
+		emphasis: "subtle",
+		color: "magenta",
+		children: "Classified"
+	}
+};
+
+export const bold: Story = {
+	name: "Bold",
+	args: {
+		size: "sm",
+		rounded: true,
+		emphasis: "bold",
+		color: "magenta",
+		children: "Classified"
+	}
+};
