b5b98bc63471104244bfb7e16d5cf26917b16555
Atzin Escandia <atzin.ext@zenml.io>
2024-04-11 09:56:20 +0200

    (feature/add-dialog): add icon close

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -57,7 +57,20 @@ const DialogHeader = ({ className, children, ...props }: React.HTMLAttributes<HT
 	>
 		{children}
 		<DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-			{/* <Close className="h-6 w-6 fill-neutral-500" /> */}
+			<svg
+				width="24px"
+				height="24px"
+				viewBox="0 0 24 24"
+				fill="#6B7280"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					fillRule="evenodd"
+					clipRule="evenodd"
+					d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
+				/>
+			</svg>
+
 			<span className="sr-only">Close</span>
 		</DialogPrimitive.Close>
 	</div>
